9c234a277a613d7a6d30d9e3f777404bfab491ce
Kieu <kieuphan530@gmail.com>
2021-09-23 01:09:46 -0500

    commit tree d21e24fc7d6a81754639cce3beb44248aff5e2c3

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+let pickup = 42;
+
+function distanceFromHqInBlocks(start) {
+    if (start > pickup) {
+        return start - pickup
+    } else if (start < pickup) {
+        return pickup - start
+    }
+}
+
+let feet = 264;
+function distanceFromHqInFeet(start) {
+    return distanceFromHqInBlocks(start) * feet
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * feet
+    } else if (start < destination) {
+        return (destination - start) * feet
+    }
+}
+
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start,destination);
+    if (distance < 400) {
+        return 0
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02
+    } else if (distance > 2000 && distance < 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
